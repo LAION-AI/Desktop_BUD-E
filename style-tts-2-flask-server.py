cef0bc6e619960c9a9acede0009ee2bad029e2b8
Christoph Schuhmann <christoph_s@freenet.de>
2024-07-03 09:34:16 +0200

    Update style-tts-2-flask-server.py

--- style-tts-2-flask-server.py
+++ style-tts-2-flask-server.py
@@ -1,3 +1,17 @@
+"""
+dependencies:
+
+git clone https://github.com/yl4579/StyleTTS2.git
+cd StyleTTS2
+pip install SoundFile torchaudio munch torch pydub pyyaml librosa nltk matplotlib accelerate transformers phonemizer einops einops-exts tqdm typing-extensions git+https://github.com/resemble-ai/monotonic_align.git
+sudo apt-get install espeak-ng
+git-lfs clone https://huggingface.co/yl4579/StyleTTS2-LJSpeech
+mv StyleTTS2-LJSpeech/Models .
+pip install flask StyleTTS2
+pip install SoundFile torchaudio munch torch pydub pyyaml librosa nltk matplotlib accelerate transformers phonemizer einops einops-exts tqdm typing-extensions git+https://github.com/resemble-ai/monotonic_align.git
+"""
+
+
 from flask import Flask, request, jsonify
 from flask import send_from_directory
 import threading
